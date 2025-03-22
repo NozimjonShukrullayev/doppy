a644095be7586d0abb45de39163a119091c304ea
Nozimjon Shukrullayev <nozimshukrullayev@gmail.com>
2025-03-22 17:37:01 +0500

    container padding changed

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,22 +1,22 @@
-import { getIndustryInsights } from "@/actions/dashboard";
-import DashboardView from "./_component/dashboard-view";
-import { getUserOnboardingStatus } from "@/actions/user";
-import { redirect } from "next/navigation";
+import { getIndustryInsights } from '@/actions/dashboard'
+import { getUserOnboardingStatus } from '@/actions/user'
+import { redirect } from 'next/navigation'
+import DashboardView from './_component/dashboard-view'
 
 export default async function DashboardPage() {
-  const { isOnboarded } = await getUserOnboardingStatus();
+	const { isOnboarded } = await getUserOnboardingStatus()
 
-  // If not onboarded, redirect to onboarding page
-  // Skip this check if already on the onboarding page
-  if (!isOnboarded) {
-    redirect("/onboarding");
-  }
+	// If not onboarded, redirect to onboarding page
+	// Skip this check if already on the onboarding page
+	if (!isOnboarded) {
+		redirect('/onboarding')
+	}
 
-  const insights = await getIndustryInsights();
+	const insights = await getIndustryInsights()
 
-  return (
-    <div className="container mx-auto">
-      <DashboardView insights={insights} />
-    </div>
-  );
+	return (
+		<div className='container px-4 mx-auto'>
+			<DashboardView insights={insights} />
+		</div>
+	)
 }
